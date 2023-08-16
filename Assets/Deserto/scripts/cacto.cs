b7187d7d5115810c8435cd0acfc0878956058984
csab85 <csantanna998@gmail.com>
2023-08-16 08:26:58 -0300

    aaaa

---
+++ Assets/Deserto/scripts/cacto.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cacto : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float movement;
+    float speed = 0.003f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed, 0, 0);
+        movement += Mathf.Abs(speed);
+
+        if (movement > 3)
+        {
+            movement = 0;
+            speed *= -1;
+            transform.localScale = Vector3.Scale(transform.localScale, new Vector3(-1, 1, 1));
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if(collider.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+            MonoBehaviour.print("aaaaaaaaaaa");
+        }
+    }
+}
