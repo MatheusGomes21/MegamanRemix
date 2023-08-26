93a61facc20c602687aa10bcc0075c2c9fb646b1
MatheusGomes21 <egsmatheus@gmail.com>
2023-08-26 17:06:12 -0300

    Disco Voador segue o cowboy e mata ele

---
+++ Assets/Deserto/scripts/InimigoSeguindo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoSeguindo : MonoBehaviour
+{
+    public float Speed;
+    private Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);   
+    }
+}
