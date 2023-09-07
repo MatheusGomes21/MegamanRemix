5751a330ad021e8361090c92ccf775f1efa7e6ba
csab85 <csantanna998@gmail.com>
2023-09-07 16:16:32 -0300

    tiro e recarga prontos (quase)

--- Assets/Deserto/scripts/Municao.cs
+++ Assets/Deserto/scripts/Municao.cs
@@ -6,12 +6,28 @@ public class Municao : MonoBehaviour
 {
     public List<GameObject> recargas;
     public int cargas = 5;
+    GameObject mira;
 
+    //funcao recarregar
+    IEnumerator Recarregar()
+    {
+        mira.GetComponent<Animator>().SetBool("Recarregando", true);
+        for (int i = 0; i <= 5; i++)
+        {
+            yield return new WaitForSeconds(0.33f);
+            
+            if (cargas < i)
+            {
+                cargas = i;
+            }
+        }
+        mira.GetComponent<Animator>().SetBool("Recarregando", false);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        mira = GameObject.Find("Mira");
     }
 
     // Update is called once per frame
@@ -20,7 +36,7 @@ public class Municao : MonoBehaviour
         //nao rodar
         transform.rotation = Quaternion.Euler(0, 0, 0);
 
-        //carregar? sla
+        //atualizar display de balas
         foreach(GameObject recharge in recargas)
         {
             recharge.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0);
@@ -31,12 +47,20 @@ public class Municao : MonoBehaviour
             }
 
             recharge.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1);
-            MonoBehaviour.print(cargas);
         }
 
+        //descarregar/atirar
         if (Input.GetButtonDown("Fire1"))
         {
             cargas -= 1;
+        }
+
+        //recarregar
+        if (Input.GetButtonDown("Recarga"))
+        {
+            StartCoroutine(Recarregar());
+            
+
         }
     }
 }
