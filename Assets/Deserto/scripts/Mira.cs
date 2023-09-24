0bda3ddeb55d9dda6b506c26965779f2a241a0cf
csab85 <csantanna998@gmail.com>
2023-09-24 14:44:15 -0300

    ashuashaushashu

--- Assets/Deserto/scripts/Mira.cs
+++ Assets/Deserto/scripts/Mira.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mira : MonoBehaviour
+public class Mira : MonoBehaviour
 {
     public Animator animator;
     public Animator fxAnimator;
